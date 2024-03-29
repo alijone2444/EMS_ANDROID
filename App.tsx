efbfc32492e9103bc1791229706e008d982fd447
alijone2444 <alijone2444@gmail.ccom>
2024-03-29 14:15:33 +0500

    .

--- App.tsx
+++ App.tsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StyleSheet, ImageBackground, Text, Image } from 'react-native';
 import SigninSignoutOption from './src/containers/signin_signup_screens/signin_signup_option_screen';
 import SignInScreen from './src/containers/signin_signup_screens/signin_screen';
 import SignUpScreen from './src/containers/signin_signup_screens/signup_screen';
 import { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
 const App = () => {
   const [currentScreen, setCurrectScreen] = useState('SignUp-SignIn')
+
+  const Stack = createNativeStackNavigator();
+  const [student, setStudent] = useState({ ID: 202, REG: 322221, department: 'cs' })
+
+
+  useEffect(() => {
+    setStudent({ ...student, REG: student.REG + 1 });
+    console.log(student)
+  }, [])
   return (
-    <View>
-      {currentScreen === 'SignUp-SignIn' && <SigninSignoutOption />}
-      <SignInScreen />
-      <SignUpScreen />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="SigninSignoutOption"
+          component={SigninSignoutOption}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="SignInScreen" options={{ headerShown: false }} component={SignInScreen} />
+        <Stack.Screen name="SignUpScreen" options={{ headerShown: false }} component={SignUpScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
--- src/containers/signin_signup_screens/signin_signup_option_screen.js
+++ src/containers/signin_signup_screens/signin_signup_option_screen.js
@@ -1,76 +1,84 @@
 
-import { View ,ImageBackground,StyleSheet, Text,Dimensions,Image, Button ,TouchableOpacity  } from 'react-native';
+import { View, ImageBackground, StyleSheet, Text, Dimensions, Image, Button, TouchableOpacity } from 'react-native';
 // import Svg, { Path } from 'react-native-svg';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-function SigninSignoutOption(){
-    return(
-        <View>
-            <ImageBackground 
-                source={require('../../resources/signIn_sign_out_background.jpg')}
+function SigninSignoutOption({ navigation }) {
+    return (
+        <View style={{
+            flex: 1,
+        }}>
+            <ImageBackground
+                source={require('../../resources/background.jpg')}
                 style={styles.background}
                 resizeMode="stretch" >
             </ImageBackground>
             <View style={styles.overlay}></View>
-                <Text style={styles.welcome}>Welcome Back</Text>
-                <View style={styles.ContainerLoginLogout}>               
+            <View style={styles.ContainerLoginLogout}>
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                     <Image
                         source={require('../../resources/ist-img-edited.jpg')}
                         style={styles.istImg}
                         resizeMode="stretch"
-                        />
-                    <Text style={{fontSize:17,fontWeight:'bold'}}>Institute Of Space And Technology</Text>
+                    />
+
+                    <Text style={{ fontSize: 17, fontWeight: 'bold' }}>Institute Of Space And Technology</Text>
                     <Text>Access Education Gateway</Text>
-                    <View style={styles.buttonsGroup}>   
-                        <TouchableOpacity
-                            activeOpacity={0.6}
-                            style={[styles.btn, { backgroundColor: '#ffff' }]}
-                            onPress={() => {
-                            // Handle Sign In
-                            }}
-                        >
-                        <Text style={{ color: 'black',textAlign:'center' }}>Sign In</Text>
-                        </TouchableOpacity>
+                </View>
+
+                <View style={styles.buttonsGroup}>
+                    <TouchableOpacity
+                        activeOpacity={0.6}
+                        style={[styles.btn, { backgroundColor: '#ffff' }]}
+
+                        onPress={() => {
+                            navigation.navigate('SignInScreen', { name: 'Jane' })
+                        }}
+                    >
+                        <Text style={{ color: 'black', textAlign: 'center' }}>Sign In</Text>
+                    </TouchableOpacity>
                     <View style={{ height: 15 }}></View>
-                        <TouchableOpacity
-                            activeOpacity={0.6}
-                            style={[styles.btn, { backgroundColor: 'transparent',
-                            borderWidth: 1,borderColor: "white" }]}
-                            onPress={() => {
-                            // Handle Sign Up
-                            }}
-                        >
-                        <Text style={{ color: 'white',textAlign:'center' }}>Sign Up</Text>
-                        </TouchableOpacity>
-                    </View>
+                    <TouchableOpacity
+                        activeOpacity={0.6}
+                        style={[styles.btn, {
+                            backgroundColor: 'transparent',
+                            borderWidth: 1, borderColor: "white"
+                        }]}
+                        onPress={() => {
+                            navigation.navigate('SignUpScreen', { name: 'Jane' })
+                        }}
+                    >
+                        <Text style={{ color: 'white', textAlign: 'center' }}>Sign Up</Text>
+                    </TouchableOpacity>
                 </View>
-                <View style={styles.socialMedia}>
-                    <Text>Sign in with social media</Text>
-                    <View style={styles.iconContainer}>
-                        <TouchableOpacity onPress={() => {/* Handle social media login */}}>
-                            <View style={{margin:'2%'}}>
-                                <Icon name="facebook" size={30} color="white" />
-                            </View>
-                        </TouchableOpacity>
-                        <TouchableOpacity onPress={() => {/* Handle social media login */}}>
-                            <View style={{margin:'2%'}}>
-                                <Icon name="twitter" size={30} color="white" />
-                            </View>
-                        </TouchableOpacity>
-                        <TouchableOpacity onPress={() => {/* Handle social media login */}}
-                        >
-                            <View style={{margin:'2%'}}>
-                                <Icon name="linkedin" size={30} color="white" />
-                            </View>
-                        </TouchableOpacity>
-                    </View>
+            </View>
+            <View style={styles.socialMedia}>
+                <Text>Sign in with social media</Text>
+                <View style={styles.iconContainer}>
+                    <TouchableOpacity onPress={() => {/* Handle social media login */ }}>
+                        <View style={{ margin: '2%' }}>
+                            <Icon name="facebook" size={30} color="white" />
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => {/* Handle social media login */ }}>
+                        <View style={{ margin: '2%' }}>
+                            <Icon name="twitter" size={30} color="white" />
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => {/* Handle social media login */ }}
+                    >
+                        <View style={{ margin: '2%' }}>
+                            <Icon name="linkedin" size={30} color="white" />
+                        </View>
+                    </TouchableOpacity>
                 </View>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    background:{
+    background: {
         width: Dimensions.get('window').width,
         height: Dimensions.get('window').height,
         position: "absolute",
@@ -81,50 +89,52 @@ const styles = StyleSheet.create({
         zIndex: -1
     },
     overlay: {
-        position:'absolute',
+        position: 'absolute',
         width: Dimensions.get('window').width,
         height: Dimensions.get('window').height,
         backgroundColor: 'rgba(0, 0, 0, 0.2)',
-      },
-    ContainerLoginLogout:{
-        width:'100%',
-        display:'flex',
-        alignItems:'center',
     },
-    istImg:{
-        width: '40%',
-        height: '20%',
-        marginVertical:'10%'
+    ContainerLoginLogout: {
+        width: '100%',
+        flex: 1,
+        alignItems: 'center',
+        marginVertical: '30%'
     },
-    welcome:{
-        fontFamily:'Roboto-Bold',
-        fontSize:30,
-        color:'white',
-        paddingTop:'15%',
-        paddingBottom:'15%',
-        textAlign:'center',
+    istImg: {
+        width: 200,
+        height: 120,
+        marginVertical: '10%'
+    },
+    welcome: {
+        fontFamily: 'Roboto-Bold',
+        fontSize: 30,
+        color: 'white',
+        paddingTop: '15%',
+        paddingBottom: '15%',
+        textAlign: 'center',
 
-      },
-      buttonsGroup:{
-        width:'70%',
-        display:'flex',
-        flexDirection:'column',
-        marginVertical:'10%'
-      },
-      btn:{
-        borderRadius:20,
+    },
+    buttonsGroup: {
+        width: '70%',
+        flex: 1,
+        justifyContent: 'center',
+        flexDirection: 'column',
+        marginVertical: '10%'
+    },
+    btn: {
+        borderRadius: 20,
         overflow: 'hidden',
-        padding:"6%",
-      },
-      socialMedia:{
-        display:'flex',
-        alignItems:'center',
-      },
-      iconContainer:{
-        display:'flex',
-        flexDirection:'row',
-        paddingVertical:'2%',
-      }
-  });
+        padding: "6%",
+    },
+    socialMedia: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+    iconContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        paddingVertical: '2%',
+    }
+});
 
 export default SigninSignoutOption
